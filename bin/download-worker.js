7060af28477de76ff725334b61a7e4ae83674ccf
Jose Ospina <jospina@hugeinc.com>
2023-03-07 10:31:48 -0500

    Updated how we're iterating over urls in download-worker.js

--- bin/download-worker.js
+++ bin/download-worker.js
@@ -1,3 +1,5 @@
+// download-worker.js
+
 import axios from 'axios';
 import fs from 'fs';
 import { parentPort } from 'worker_threads';
@@ -5,15 +7,14 @@ import path from 'path';
 import { pipeline } from 'stream/promises';
 
 const downloadImage = async (imagesUrl, output) => {
-  imagesUrl.map(async (imageUrl) => {
+  for (const imageUrl of imagesUrl) {
     const response = await axios.get(imageUrl, { responseType: 'stream' });
     const contentType = response.headers['content-type'];
     const fileExt = contentType.split('/')[1];
     const fileName = path.basename(imageUrl, path.extname(imageUrl));
-
     const dest = fs.createWriteStream(`${output}/${fileName}.${fileExt}`);
     await pipeline(response.data, dest);
-  });
+  }
 };
 
 const validateDirectory = (directory) => {
